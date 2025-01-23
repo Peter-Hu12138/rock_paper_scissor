fbfec67eddc354081e97ae39acdd8ed9e6cd0490
PeterHu-12138 <13699790394@163.com>
2025-01-23 10:14:04 +0800

    wrap playRound function in the playGame function, which now records scores for both sides and repeats playRound 5 times

--- index.js
+++ index.js
@@ -27,47 +27,52 @@ function getHumanChoice(){
         return "S"
     }
 }
-
-function playRound(){
-    const choices = ["R", "S", "P"]
-    const humanChoice = getHumanChoice()
-    const computerChoice = getComputerChoice()
-    const humanIndex = choices.indexOf(humanChoice)
-    console.log(`you choice ${humanChoice}, computer choice: ${computerChoice}`)
-    if(humanChoice == computerChoice){
-        console.log("tie")
+function playGame(){
+    function playRound(){
+        const choices = ["R", "S", "P"]
+        const humanChoice = getHumanChoice()
+        const computerChoice = getComputerChoice()
+        const humanIndex = choices.indexOf(humanChoice)
+        console.log(`you choice ${humanChoice}, computer choice: ${computerChoice}`)
+        if(humanChoice == computerChoice){
+            console.log("tie")
+        }
+        else if(choices[(humanIndex + 1) % 3] == computerChoice){
+            console.log("you win")
+            human_score += 1
+        }
+        else{
+            console.log("you lose")
+            computer_score += 1
+        }
     }
-    else if(choices[(humanIndex + 1) % 3] == computerChoice){
-        console.log("you win")
-        human_score += 1
+    let human_score = 0
+    let computer_score = 0
+    for(let i = 0; i < 5; i++){
+        playRound()
     }
-    else{
-        console.log("you lose")
-        computer_score += 1
-    }
+    console.log(`Your score: ${human_score} Computer score: ${computer_score}`)
 }
 
-function playRoundTesting(humanChoice, computerChoice){
-    const choices = ["R", "S", "P"]
-    const humanIndex = choices.indexOf(humanChoice)
-    console.log(`you choice ${humanChoice}, computer choice: ${computerChoice}`)
-    if(humanChoice == computerChoice){
-        console.log("tie")
-    }
-    else if(choices[(humanIndex + 1) % 3] == computerChoice){
-        console.log("you win")
-        human_score += 1
-    }
-    else{
-        console.log("you lose")
-        computer_score += 1
-    }
-}
 
-let human_score = 0
-let computer_score = 0
+// function playRoundTesting(humanChoice, computerChoice){
+//     const choices = ["R", "S", "P"]
+//     const humanIndex = choices.indexOf(humanChoice)
+//     console.log(`you choice ${humanChoice}, computer choice: ${computerChoice}`)
+//     if(humanChoice == computerChoice){
+//         console.log("tie")
+//     }
+//     else if(choices[(humanIndex + 1) % 3] == computerChoice){
+//         console.log("you win")
+//         human_score += 1
+//     }
+//     else{
+//         console.log("you lose")
+//         computer_score += 1
+//     }
+// }
 
 
 
 
-console.log("Hello world")
+playGame()

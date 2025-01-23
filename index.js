7c14ce7660c1d4ac2e5e828211bc77000d886323
PeterHu-12138 <13699790394@163.com>
2025-01-23 10:05:49 +0800

    add human interface to get user input and a wrapper function for playing a round

--- index.js
+++ index.js
@@ -2,16 +2,71 @@ function getComputerChoice(){
     let num = Math.random() * 3;
     
     if(num < 1){
-        return("rock");
+        return("R")
     }
     else if(num < 2){
-        return("paper");
+        return("P")
     }
     else{
-        return("scissors");
+        return("S")
     }
-
 }
+
+function getHumanChoice(){
+    let choice = prompt(`Hey, give your choice:
+        "1" for Rock
+        "2" Paper
+        "3" Scissor:`)
+    if(choice == "1" || choice == "rock"){
+        return "R"
+    }
+    else if(choice == "2" || choice == "paper"){
+        return "P"
+    }
+    else{
+        return "S"
+    }
+}
+
+function playRound(){
+    const choices = ["R", "S", "P"]
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+    const humanIndex = choices.indexOf(humanChoice)
+    console.log(`you choice ${humanChoice}, computer choice: ${computerChoice}`)
+    if(humanChoice == computerChoice){
+        console.log("tie")
+    }
+    else if(choices[(humanIndex + 1) % 3] == computerChoice){
+        console.log("you win")
+        human_score += 1
+    }
+    else{
+        console.log("you lose")
+        computer_score += 1
+    }
+}
+
+function playRoundTesting(humanChoice, computerChoice){
+    const choices = ["R", "S", "P"]
+    const humanIndex = choices.indexOf(humanChoice)
+    console.log(`you choice ${humanChoice}, computer choice: ${computerChoice}`)
+    if(humanChoice == computerChoice){
+        console.log("tie")
+    }
+    else if(choices[(humanIndex + 1) % 3] == computerChoice){
+        console.log("you win")
+        human_score += 1
+    }
+    else{
+        console.log("you lose")
+        computer_score += 1
+    }
+}
+
+let human_score = 0
+let computer_score = 0
+
 
 
 
